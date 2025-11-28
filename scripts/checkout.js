4943d90f06a481e42ee59bef2958f28776492c3b
niccolosegato <segato117@gmail.com>
2025-11-28 14:30:44 +0100

    First poster ok

---
+++ scripts/checkout.js
@@ -0,0 +1,8 @@
+/**
+ * checkout.js
+ * 
+ * This script handles the functionality of the checkout page
+ * 
+ * Created by Niccolò Segato
+ * Copyright © 2025 Niccolò Segato. All rights reserved.
+ */
